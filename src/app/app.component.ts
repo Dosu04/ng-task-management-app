d98bab66fdd99a0257b7bf62e0c2292d1e704b44
Emmanuel Oluwafunso Oladosu <87922887+Dosu04@users.noreply.github.com>
2023-05-26 03:34:42 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+
+  taskArray = [{taskName: 'Code practice', isDone: false}]
+
+  onSubmit(form: NgForm){
+    
+    this.taskArray.push({
+      taskName: form.controls['task'].value,
+      isDone: false
+    })
+
+    form.reset();
+  }
+
+  onDelete(index: number){
+    this.taskArray.splice(index, 1);
+  }
+
+  onCheck(index: number){
+    this.taskArray[index].isDone = !this.taskArray[index].isDone;
+  }
+
 }
